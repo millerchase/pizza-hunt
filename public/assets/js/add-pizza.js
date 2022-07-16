7ccdb563f9a0db4b78d0b4423b55ce3c75ed4571
CMiller <mlr.chase40@gmail.com>
2022-07-15 23:12:38 -0500

    add Offline functionality

--- public/assets/js/add-pizza.js
+++ public/assets/js/add-pizza.js
@@ -61,7 +61,10 @@ const handlePizzaSubmit = event => {
       alert('Pizza created successfully!');
       console.log(postResponse);
     })
-    .catch(err => console.log(err));
+    .catch(err => {
+      console.log(err);
+      saveRecord(formData);
+    });
 };
 
 $pizzaForm.addEventListener('submit', handlePizzaSubmit);
